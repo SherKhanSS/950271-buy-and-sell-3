28b4d4fd70a7820a01bf7ee92be55dca2a60db96
SherKhan <gurovg@gmail.com>
2020-05-21 14:23:43 +0300

    3-й модуль вторая часть - добавляет шаблоны

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
-offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
-offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`/offers/category/:id`));
+offersRouter.get(`/add`, (req, res) => res.render(`/offers/add`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`/offers/edit/:id`));
+offersRouter.get(`/:id`, (req, res) => res.render(`/offers/:id`));
 
 module.exports = offersRouter;
