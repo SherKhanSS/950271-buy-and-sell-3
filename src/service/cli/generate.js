93076192739332ee6729b9435400c0201991cc3d
Yuriy <gurovg@gmail.com>
2021-02-12 17:21:39 +0300

    start module

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,18 +2,23 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
+const {nanoid} = require(`nanoid`);
 
 const {
   getRandomInt,
   shuffle,
 } = require(`../../utils`);
 
+const {MAX_ID_LENGTH} = require(`../../constants`);
+
 const DEFAULT_COUNT = 1;
+const MAX_COMMENTS = 4;
 const FILE_NAME = `mocks.json`;
 
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
 const OfferType = {
   offer: `offer`,
@@ -42,16 +47,31 @@ const readContent = async (filePath) => {
 
 const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
 
-const generateOffers = (count, titles, categories, sentences) => (
-  Array(count).fill({}).map(() => ({
-    category: [categories[getRandomInt(0, categories.length - 1)]],
-    description: shuffle(sentences).slice(1, 5).join(` `),
-    picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    title: titles[getRandomInt(0, titles.length - 1)],
-    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
-    sum: getRandomInt(SumRestrict.min, SumRestrict.max),
-  }))
-);
+const generateComments = (count, comments) =>
+  Array(count)
+    .fill({})
+    .map(() => ({
+      id: nanoid(MAX_ID_LENGTH),
+      text: shuffle(comments).slice(0, getRandomInt(1, 3)).join(` `),
+    }));
+
+const generateOffers = (count, titles, categories, sentences, comments) =>
+  Array(count)
+    .fill({})
+    .map(() => ({
+      id: nanoid(MAX_ID_LENGTH),
+      category: [categories[getRandomInt(0, categories.length - 1)]],
+      description: shuffle(sentences).slice(1, 5).join(` `),
+      picture: getPictureFileName(
+          getRandomInt(PictureRestrict.min, PictureRestrict.max)
+      ),
+      title: titles[getRandomInt(0, titles.length - 1)],
+      type: Object.keys(OfferType)[
+        Math.floor(Math.random() * Object.keys(OfferType).length)
+      ],
+      sum: getRandomInt(SumRestrict.min, SumRestrict.max),
+      comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
+    }));
 
 module.exports = {
   name: `--generate`,
@@ -59,6 +79,7 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
@@ -68,7 +89,9 @@ module.exports = {
       return;
     }
 
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(
+        generateOffers(countOffer, titles, categories, sentences, comments)
+    );
 
     try {
       await fs.writeFile(FILE_NAME, content);
